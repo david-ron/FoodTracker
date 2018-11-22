1f8a30a2bfcb4b652fbe0b49a87c1b9f34796779
david-ron <ronci.david@gmail.com>
2018-11-22 00:48:36 -0500

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by David Ronci on 2018-11-22.
+//  Copyright © 2018 David Ronci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
